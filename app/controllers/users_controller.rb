329c1b3856643109035613b1297452a52ee21b58
EC2 Default User <ec2-user@ip-172-31-33-235.ap-northeast-1.compute.internal>
2021-03-18 03:30:30 +0000

    コントローラーにfacebook_loginメソッド作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,9 +65,22 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   
-  def meals_input
+  def facebook_login
+  @user = User.from_omniauth(request.env["omniauth.auth"])
+    result = @user.save(context: :facebook_login)
+    if result
+      log_in @user
+      redirect_to @user
+    else
+      redirect_to auth_failure_path
+    end
   end
   
+  #認証に失敗した際の処理
+  def auth_failure 
+    @user = User.new
+    render '任意のアクション'
+  end
 private
 
     def user_params
